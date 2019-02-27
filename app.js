552c96cb2740d830b3cc8f8c642afcf3f44a4d93
qizr <qzr_1986@163.com>
2019-02-27 23:41:03 +0800

    test

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
+
+
 app.get('/', (req, res) => res.send('Hello World from ailiyun! qzr_1986@163.com'))
+app.use('/prototype',express.static('/Users/qizhuoran/Desktop/prototype'))
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
